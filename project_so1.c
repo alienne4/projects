f6eba7979c612c8b8591a7041a6c86a2eec75156
Alice <alicecania@gmail.com>
2024-04-30 20:57:00 +0300

    milestone 2 done

--- project_so1.c
+++ project_so1.c
@@ -14,7 +14,7 @@ void openDirectories(char *directorPath)
 
     if(director==NULL)
     {
-        printf("Acesta este un mesaj de eroare");
+        printf("Acesta este un mesaj de eroaore");
         return;
     }
 
@@ -57,16 +57,24 @@ void openDirectories(char *directorPath)
 
                 char snapshotContent[1024] = "";
 
-                sprintf(snapshotContent, "Entry: %s\nSize: %d\nInode number: %d\n", filename, buf.st_size, buf.st_ino);
+                char filePermissions[11];
+
+                filePermissions[0] = '-';
+                filePermissions[1] = (buf.st_mode & S_IRUSR) ? 'r' : '-';
+                filePermissions[2] = (buf.st_mode & S_IWUSR) ? 'w' : '-';
+                filePermissions[3] = (buf.st_mode & S_IXUSR) ? 'x' : '-';
+                filePermissions[4] = (buf.st_mode & S_IRGRP) ? 'r' : '-';
+                filePermissions[5] = (buf.st_mode & S_IWGRP) ? 'w' : '-';
+                filePermissions[6] = (buf.st_mode & S_IXGRP) ? 'x' : '-';
+                filePermissions[7] = (buf.st_mode & S_IROTH) ? 'r' : '-';
+                filePermissions[8] = (buf.st_mode & S_IWOTH) ? 'w' : '-';
+                filePermissions[9] = (buf.st_mode & S_IXOTH) ? 'x' : '-';
+                filePermissions[10] = '\0';
+   
+
+                sprintf(snapshotContent, "Timestamp: %lld\nEntry: %s\nSize: %d\nLast Modified: %lld\nPermissions: %s\nInode number: %d\n", buf.st_atimespec.tv_sec, filename, buf.st_size, buf.st_mtimespec.tv_sec, filePermissions, buf.st_ino);
 
                 ssize_t result = write(fptr, &snapshotContent, strlen(snapshotContent) - 1);
-
-                // printf("Timestamp: %s\n", buf.st_atimespec.tv_sec);
-                            /// printf("Entry: %s\n", filename);
-                            ///printf("Size: %d\n", buf.st_size);
-                            // printf("Last Modified: %s\n",  buf.st_mtimespec.tv_sec);
-                            // printf("Permissions: %s\n", filename);
-                            ///printf("Inode number: %d\n", buf.st_ino);
 
                 close(fptr);
                
@@ -80,13 +88,16 @@ void openDirectories(char *directorPath)
 
 int main(int argc, char *argv[])
 {
-    if(argc != 2)
+    if(argc < 2)
     {
-        printf("Usage <exec> dir N\n");
+        printf("Usage <exec> dir1 dir2 dir3 ... N\n");
         exit(-1);
     }
 
-    openDirectories(argv[1]);
+    for (int i = 1; i < argc; i++)
+    {
+        openDirectories(argv[i]);
+    }
 
     return 0;
 }
